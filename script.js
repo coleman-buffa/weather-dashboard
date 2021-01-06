492f7e38c610a673a7090a985456af4d4a763d53
Coleman Buffa <coleman.buffa@gmail.com>
2021-01-05 19:50:02 -0800

    Add user city input capture and basic city list rendering functions.

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 // Wait for the page to load before doing anything
 $(document).ready(function() {
     //All JS goes inside here
+    
+    
+    var cityList = [];
+    //Captures the user's entered city and stores in an array for later use    
+    $("#city-button").on("click", function(event) {
+        event.preventDefault();
+
+        var city = $("#city-input").val().trim();
+        cityList.push(city);
+
+        $("#city-input").val("");
+
+        renderCityList();
+    });
+
+
+
+    function renderCityList() {
+        $("#city-list").empty();
+
+        for (var i = 0; i < cityList.length; i++) {
+            var newLi = $("<li>");
+            newLi.addClass("city-li");
+            newLi.attr("data-name", cityList[i]);
+            newLi.text(cityList[i]);
+            $("#city-list").append(newLi);
+        }
+    }
+
    
 });

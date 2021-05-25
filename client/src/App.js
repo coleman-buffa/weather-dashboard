1300f1f94900347d33c3554f2a569ff785c54f93
Coleman Buffa <coleman.buffa@gmail.com>
2021-05-25 11:31:15 -0700

    Draft and style topbar component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import { Container, Grid} from '@material-ui/core';
+import TopBar from './components/topbar/topbar.js';
+import { Container, Grid} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Container>
         <Grid container spacing={3}>
           <Grid item xs={12} md={4}>

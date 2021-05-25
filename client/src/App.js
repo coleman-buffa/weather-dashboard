ba397053106b33f1befbfd9a5e4993bb4850b80b
Coleman Buffa <coleman.buffa@gmail.com>
2021-05-25 10:33:53 -0700

    Setup broad page layout. Trim excess code from create-react-app output.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Container, Grid} from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={4}>
+            Hello World!
+          </Grid>
+          <Grid item xs={12} md={8}>
+            Hello World!
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }

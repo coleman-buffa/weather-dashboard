d25ef478dc9d5e15d6d9401a6dee53081c088d8a
Coleman Buffa <coleman.buffa@gmail.com>
2021-05-25 14:31:40 -0700

    Draft current weather component. Re-org component folder.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import TopBar from './components/topbar/topbar';
-import SearchBar from './components/searchbar/searchbar';
-import CityList from './components/citylist/citylist';
-import { Container, Grid, Paper } from "@material-ui/core";
+import TopBar from './components/topbar';
+import SearchBar from './components/searchbar';
+import CityList from './components/citylist';
+import CurrentWeather from './components/currentweather';
+import { Container, Grid } from "@material-ui/core";
 
 function App() {
   return (
@@ -11,11 +12,11 @@ function App() {
       <Container>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={4}>
-            <SearchBar/>
+            <SearchBar />
             <CityList />
           </Grid>
           <Grid item xs={12} sm={8}>
-            <Paper>Hello World</Paper>
+            <CurrentWeather />
           </Grid>
         </Grid>
       </Container>
---
+++ client/src/components/currentweather.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    '& > *': {
+      margin: theme.spacing(1),
+
+    },
+  },
+}));
+
+export default function CurrentWeather() {
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Typography>Current Weather in London:</Typography>
+        <Typography variant="caption">Temperature:</Typography>
+        <Typography variant="caption">Humidity:</Typography>
+        <Typography variant="caption">Wind Speed:</Typography>
+        <Typography variant="caption">UV Index:</Typography>
+      </Paper>
+
+    </div>
+  )
+}

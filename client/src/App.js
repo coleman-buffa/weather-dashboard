a6bbfac547aca721d8e87a61094c0aba987be4d9
Coleman Buffa <coleman.buffa@gmail.com>
2021-05-25 13:43:22 -0700

    Draft and style citylist component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import TopBar from './components/topbar/topbar';
 import SearchBar from './components/searchbar/searchbar';
+import CityList from './components/citylist/citylist';
 import { Container, Grid, Paper } from "@material-ui/core";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Grid container spacing={3}>
           <Grid item xs={12} sm={4}>
             <SearchBar/>
+            <CityList />
           </Grid>
           <Grid item xs={12} sm={8}>
             <Paper>Hello World</Paper>
---
+++ client/src/components/citylist/citylist.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ButtonGroup, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function CityList() {
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <ButtonGroup
+        orientation='vertical'
+        color='primary'
+        aria-label='vertical contained primary button group'
+        variant='text'
+      >
+        <Button>London</Button>
+        <Button>Berlin</Button>
+        <Button>San Francisco</Button>
+        <Button>New York</Button>
+      </ButtonGroup>
+
+    </div>
+  )
+}

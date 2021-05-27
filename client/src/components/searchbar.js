c635a06e45bfa0a4165cda2ecbca75bc3b98d2b0
Coleman Buffa <coleman.buffa@gmail.com>
2021-05-26 18:55:35 -0700

    Add search icon to search bar.

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { IconButton, InputAdornment } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,6 +25,15 @@ export default function SearchBar(props) {
         varient="filled"
         type="search"
         inputRef={props.inputRef}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={props.handleInputChange}>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          )
+        }}
       />
     </form>
   )

913dab7567023fc5477cdd0050ef295cbac44cbd
Coleman Buffa <coleman.buffa@gmail.com>
2021-06-08 11:38:13 -0700

    Add comments.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const moment = require("moment");
 
 dotenv.config();
 
-//TODO setup routes to receive request from front end which triggers a fetch to open weather API to get the searched weather
+//Get weather data from API. First fetch is to get coords of requested city then use those coords to get current and forecasted weather information. Also modifies the timestamp to human readable version and splices 
 router.get("/weather/:city", (req, res) => {
 
   let city = req.params.city;
@@ -19,19 +19,19 @@ router.get("/weather/:city", (req, res) => {
       let lat = response.data.coord.lat;
       let lon = response.data.coord.lon;
       URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${API_KEY}`;
-      console.log(URL);
       axios({
         method: 'GET',
         url: URL
       })
         .then(response => {
           response.data.daily.map(item => {
+            //Convert unix timestamp into human readable format
             let timeStamp = moment.unix(item.dt).format("MM/DD/YYYY");
             item.dt = timeStamp;
 
+            //Add icon URL key to response object
             let iconURL = `http://openweathermap.org/img/wn/${item.weather[0].icon}.png`;
             item.weather[0].iconURL = iconURL;
-            console.log(item.weather[0].iconURL);
           })
           res.json(response.data);
         })
